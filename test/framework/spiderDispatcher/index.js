57c3d2859d23fee771f44c1f9397c5b9b854692c
leekangtaqi <leekangtaqi@sina.com>
2017-07-05 20:28:45 +0800

    add test

--- test/framework/spiderDispatcher/index.js
+++ test/framework/spiderDispatcher/index.js
@@ -4,9 +4,7 @@ import { expect } from 'chai'
 
 describe('dispatch', () => {
   it('#request spide ctrip, fill receive event login request', done => {
-    let code = '111'
     try {
-      // define message flow
       dispatcher.request(types.CTRIP_WEB_TOLOGIN, { key: '111', payload: '222' }, function(err, data){
         expect(data.action).to.be.equal('ctripWebNeedlogin')
         dispatcher.request(types.CTRIP_WEB_LOGIN, { key: '111', payload: { username: '15210383276', password: '40115891r', vcode : '555' }}, function(err, data) {
